4050441bf3bc7cffe34b31b45ffc7a283abc08a2
RN1216 <reunnoor@gmail.com>
2022-10-27 17:10:31 +0600

    course detail complite

--- src/Pages/Courses/Courses/Courses.js
+++ src/Pages/Courses/Courses/Courses.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import  Card  from 'react-bootstrap/Card';
+import EachCourseDetail from '../../SharedPage/EachCourseDetail/EachCourseDetail';
+
 
 const Courses = () => {
     const similerCourses = useLoaderData();
+    
     return (
         <div>
             <h2>Some Courses is here :{similerCourses.length}</h2>
+            {
+                similerCourses.map(courses=><EachCourseDetail
+                key={courses._id}
+                courses ={courses}
+                ></EachCourseDetail>)
+            }
         </div>
     );
 };
--- src/Pages/SharedPage/EachCourseDetail/EachCourseDetail.js
+++ src/Pages/SharedPage/EachCourseDetail/EachCourseDetail.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const EachCourseDetail = ({courses}) => {
+    const {_id,title,image_url,details}= courses;
     return (
-        <div>
-            
-        </div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+          {details}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     );
 };
 
